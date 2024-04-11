b86ea380779e13bfd08c36cb8924216361332ccb
Oana S. <oana.sasaran@cososys.com>
2024-04-11 15:01:41 +0300

    ADDED Pages structure and FIXING formating issues

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import "./globals.css";
 import { Routes, Route } from "react-router-dom";
 
-import { Home } from "./_root/pages";
+import {
+    AllUsers,
+    CreatePost,
+    EditPost,
+    Explore,
+    Home,
+    PostDetails,
+    Profile,
+    Saved,
+    UpdateProfile,
+} from "./_root/pages";
 import SigninForm from "./_auth/forms/SigninForm";
 import SignupForm from "./_auth/forms/SignupForm";
 import AuthLayout from "./_auth/AuthLayout";
@@ -20,6 +30,17 @@ const App = () => {
 
                     <Route element={<RootLayout />}>
                         <Route index element={<Home />} />
+                        <Route path="/explore" element={<Explore />} />
+                        <Route path="/saved" element={<Saved />} />
+                        <Route path="/all-users" element={<AllUsers />} />
+                        <Route path="/create-post" element={<CreatePost />} />
+                        <Route path="/update-post/:id" element={<EditPost />} />
+                        <Route path="/posts/:id" element={<PostDetails />} />
+                        <Route path="/profile/:id/*" element={<Profile />} />
+                        <Route
+                            path="/update-profile/:id"
+                            element={<UpdateProfile />}
+                        />
                     </Route>
                 </Routes>
 
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,83 +1,86 @@
-import { getCurrentUser } from '@/lib/appwrite/api';
-import { IContextType, IUser } from '@/types';
-import { createContext, useContext, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { getCurrentUser } from "@/lib/appwrite/api";
+import { IContextType, IUser } from "@/types";
+import { createContext, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const INITIAL_USER = {
-  id: '',
-  name: '',
-  username: '',
-  email: '',
-  imageUrl: '',
-  bio: '',
+    id: "",
+    name: "",
+    username: "",
+    email: "",
+    imageUrl: "",
+    bio: "",
 };
 
 const INITIAL_STATE = {
-  user: INITIAL_USER,
-  isLoading: false,
-  isAuthenticated: false,
-  setUser: () => { },
-  setIsAuthenticated: () => { },
-  checkAuthUser: async () => { }
-}
+    user: INITIAL_USER,
+    isLoading: false,
+    isAuthenticated: false,
+    setUser: () => {},
+    setIsAuthenticated: () => {},
+    checkAuthUser: async () => {},
+};
 
 const AuthContext = createContext<IContextType>(INITIAL_STATE);
 
 const AuthProvider = ({ children }: { children: React.ReactNode }) => {
-  
-  const [user, setUser] = useState<IUser>(INITIAL_USER);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const navigate = useNavigate();
+    const [user, setUser] = useState<IUser>(INITIAL_USER);
+    const [isLoading, setIsLoading] = useState(false);
+    const [isAuthenticated, setIsAuthenticated] = useState(false);
+    const navigate = useNavigate();
 
-  const checkAuthUser = async () => { 
-    try{
-      const currentAccount = await getCurrentUser();
+    const checkAuthUser = async () => {
+        try {
+            const currentAccount = await getCurrentUser();
 
-      if (currentAccount) {
-        setUser({
-          id: currentAccount.$id,
-          name: currentAccount.name,
-          username: currentAccount.username,
-          email: currentAccount.email,
-          imageUrl: currentAccount.imageUrl,
-          bio: currentAccount.bio
-        });
+            if (currentAccount) {
+                setUser({
+                    id: currentAccount.$id,
+                    name: currentAccount.name,
+                    username: currentAccount.username,
+                    email: currentAccount.email,
+                    imageUrl: currentAccount.imageUrl,
+                    bio: currentAccount.bio,
+                });
 
-        setIsAuthenticated(true);
+                setIsAuthenticated(true);
 
-        return true;
-      }
+                return true;
+            }
 
-      return false;
+            return false;
+        } catch (error) {
+            console.log(error);
+            return false;
+        } finally {
+            setIsLoading(false);
+        }
+    };
 
-    } catch (error) {
-      console.log(error);
-      return false;
-    } finally {
-      setIsLoading(false);
-    }
-  };
+    useEffect(() => {
+        if (
+            localStorage.getItem("cookieFallback") === null ||
+            localStorage.getItem("cookieFallback") === "[]"
+        )
+            navigate("/sign-in");
 
-  useEffect(() => {
-    
-    if (
-      localStorage.getItem('cookieFallback') === null || 
-      localStorage.getItem('cookieFallback') === '[]'
-    ) navigate('/sign-in');
+        checkAuthUser();
+    }, []);
 
-    checkAuthUser();
-    
-  }, []);
+    const value = {
+        user,
+        setUser,
+        isLoading,
+        setIsLoading,
+        isAuthenticated,
+        setIsAuthenticated,
+        checkAuthUser,
+    };
 
-  const value = { user, setUser, isLoading, setIsLoading, isAuthenticated, setIsAuthenticated, checkAuthUser}
-
-  return (
-    <AuthContext.Provider value={value}>
-      {children}  
-    </AuthContext.Provider>
-  )
-}
+    return (
+        <AuthContext.Provider value={value}>{children}</AuthContext.Provider>
+    );
+};
 
 export default AuthProvider;
 
