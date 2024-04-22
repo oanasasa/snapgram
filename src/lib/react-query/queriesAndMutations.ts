bb1c5efac131ce4e471e06518d9d3b43e5bef174
Oana <oanasasaran@yahoo.com>
2024-04-22 08:23:40 +0300

    ADDED the Create Post form functionality

--- src/lib/react-query/queriesAndMutations.ts
+++ src/lib/react-query/queriesAndMutations.ts
@@ -1,31 +1,55 @@
 import {
-    useQuery,
-    useMutation,
-    useQueryClient,
-    useInfiniteQuery,
+  useQuery,
+  useMutation,
+  useQueryClient,
+  useInfiniteQuery,
 } from "@tanstack/react-query";
+
 import {
-    createUserAccount,
-    signInAccount,
-    signOutAccount,
-} from "../appwrite/api";
-import { INewUser } from "@/types";
+  createUserAccount,
+  signInAccount,
+  getCurrentUser,
+  signOutAccount,
+  createPost,
+} from "@/lib/appwrite/api";
+import { INewPost, INewUser, IUpdatePost, IUpdateUser } from "@/types";
+import { QUERY_KEYS } from "./queriyKeys";
+
+// ============================================================
+// AUTH QUERIES
+// ============================================================
 
 export const useCreateUserAccount = () => {
-    return useMutation({
-        mutationFn: (user: INewUser) => createUserAccount(user),
-    });
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
 };
 
 export const useSignInAccount = () => {
-    return useMutation({
-        mutationFn: (user: { email: string; password: string }) =>
-            signInAccount(user),
-    });
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
+  });
 };
 
 export const useSignOutAccount = () => {
-    return useMutation({
-        mutationFn: signOutAccount,
-    });
+  return useMutation({
+    mutationFn: signOutAccount,
+  });
+};
+
+// ============================================================
+// POST QUERIES
+// ============================================================
+
+export const useCreatePost = () => {
+  const queryClient = useQueryClient();
+  return useMutation({
+    mutationFn: (post: INewPost) => createPost(post),
+    onSuccess: () => {
+      queryClient.invalidateQueries({
+        queryKey: [QUERY_KEYS.GET_RECENT_POSTS],
+      });
+    },
+  });
 };
